d8f6743ea304256e645e6a6f470cb282bb31cae9
raniagisela <raniagslp@gmail.com>
2023-12-29 09:24:45 +0700

    add fun to KontakService

--- app/src/main/java/com/example/consumerestapi/service_api/KontakService.kt
+++ app/src/main/java/com/example/consumerestapi/service_api/KontakService.kt
@@ -1,8 +1,13 @@
 package com.example.consumerestapi.service_api
 
 import com.example.consumerestapi.model.Kontak
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface KontakService {
     @Headers(
@@ -10,4 +15,16 @@ interface KontakService {
     )
     @GET("siswa.json")
     suspend fun getKontak(): List<Kontak>
+    @Headers(
+        "Accept: application/json"
+    )
+    @DELETE("/kontak/{id}")
+    suspend fun deleteKontak(@Path("id") id: Int): Response<Unit>
+
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json"
+    )
+    @POST("/kontak")
+    suspend fun insertKontak(@Body kontak: Kontak): Response<Unit>
 }
