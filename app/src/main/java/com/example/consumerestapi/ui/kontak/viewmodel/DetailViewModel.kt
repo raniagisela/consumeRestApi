4cd3433a6113f1864e8cc1ed7742931d53b8a73b
raniagisela <raniagslp@gmail.com>
2023-12-29 10:16:40 +0700

    add class and fun to DetailsViewModel

---
+++ app/src/main/java/com/example/consumerestapi/ui/kontak/viewmodel/DetailViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.consumerestapi.ui.kontak.viewmodel
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.consumerestapi.model.Kontak
+import com.example.consumerestapi.repository.KontakRepository
+import kotlinx.coroutines.launch
+
+sealed class DetailsKontakUiState{
+    data class Success(
+        val kontak: Kontak
+    ) : DetailsKontakUiState()
+
+    object Error : DetailsKontakUiState()
+    object Loading : DetailsKontakUiState()
+}
+
+class DetailsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val kontakRepository: KontakRepository
+) : ViewModel(){
+    private val kontakId: Int = checkNotNull(savedStateHandle[DetailsDestination.kontakId])
+    var detailsKontakUiState : DetailsKontakUiState by mutableStateOf(DetailsKontakUiState.Loading)
+        private set
+
+    init {
+        getKontakbyId()
+    }
+
+    fun getKontakbyId(){
+        viewModelScope.launch {
+            detailsKontakUiState = DetailsKontakUiState.Loading
+            detailsKontakUiState = try{
+                DetailsKontakUiState.Success(
+                    kontak = kontakRepository.getKontakById(kontakId)
+                )
+            } catch (e: Exception){
+                DetailsKontakUiState.Error
+            }
+        }
+    }
+}
