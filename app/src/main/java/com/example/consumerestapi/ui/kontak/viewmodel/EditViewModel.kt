9bc38b2a479d4eca6cc4547f8b918750f7e24aac
raniagisela <raniagslp@gmail.com>
2023-12-29 10:17:04 +0700

    add val and fun to EditViewModel

---
+++ app/src/main/java/com/example/consumerestapi/ui/kontak/viewmodel/EditViewModel.kt
@@ -0,0 +1,39 @@
+package com.example.consumerestapi.ui.kontak.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.consumerestapi.repository.KontakRepository
+import kotlinx.coroutines.launch
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val kontakRepository: KontakRepository
+) : ViewModel() {
+
+    var editKontakState by mutableStateOf(InsertUiState())
+        private set
+    val kontakId: Int = checkNotNull(savedStateHandle[EditDestination.kontakId])
+
+    init {
+        viewModelScope.launch {
+            editKontakState = kontakRepository.getKontakById(kontakId).toUiStateKontak()
+        }
+    }
+
+    fun updateInsertKontakState(insertUiEvent: InsertUiEvent: InsertUiEvent) {
+        editKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun updateKontak(){
+        viewModelScope.launch {
+            try {
+                kontakRepository.updateKontak(kontakId, editKontakState.insertUiEvent.toKontak())
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
